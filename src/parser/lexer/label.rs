c1b34d5a9fae20a9e8ad63bf434f9b3681a1a119
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-11-05 20:06:48 +0100

    fix: empty label

--- src/parser/lexer/label.rs
+++ src/parser/lexer/label.rs
@@ -5,7 +5,7 @@ pub struct Label {
 }
 impl Label {
     pub fn new(str: &str) -> Result<Option<Self>, String> {
-        let re = Regex::new(r"^[a-z_0-9]*:").unwrap();
+        let re = Regex::new(r"^[a-z_0-9]+:").unwrap();
         if re.captures(str).is_some() {
             let name = str.split(":").collect::<Vec<&str>>()[0].to_string();
             return Ok(Some(Label { name }));
@@ -26,6 +26,11 @@ mod tests {
         assert_eq!(new_instance.unwrap().unwrap().name, "label_test_2");
 
         let new_instance = Label::new("halt");
+
+        assert!(new_instance.is_ok());
+        assert!(new_instance.unwrap().is_none());
+
+        let new_instance = Label::new(":");
 
         assert!(new_instance.is_ok());
         assert!(new_instance.unwrap().is_none());
